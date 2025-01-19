bbc6bc11ea9b8e48c6c1b335523828b0ee6a902f
Ayenitaju Samuel <samuelayenitaju2003@gmail.com>
2025-01-19 22:26:21 +0100

    Create list for colours containing only rgb values.

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import colorgram
 
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+for i in range(30):
+    rgb = colors[i].rgb
+    red = rgb.r
+    green = rgb.g
+    blue = rgb.b
+    color_tuple = (red, green, blue)
+    rgb_colors.append(color_tuple)
 
 print(rgb_colors)
